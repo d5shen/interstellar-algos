30ec598b8ff0d9baeab85089179a9874b289f342
d5shen <darryl.shen+github@gmail.com>
2021-09-09 21:09:21 -0400

    initial setup pt 2

---
+++ src/PositionService.ts
@@ -0,0 +1,53 @@
+import { PerpService, PnlCalcOption, Position } from "./PerpService"
+import { Service } from "typedi"
+import { Log } from "./Log"
+import Big from "big.js"
+
+@Service()
+export class PositionService {
+    private readonly log = Log.getLogger(PositionService.name)
+
+    constructor(
+        readonly wallet: string,
+        readonly perpService: PerpService
+    ) {
+        
+    }
+
+    async getPerpPosition(ammAddress: string): Promise<Position> {
+        return await this.perpService.getPersonalPositionWithFundingPayment(ammAddress, this.wallet)
+    }
+
+    async getPerpUnrealizedPnl(ammAddress: string): Promise<Big> {
+        return await this.perpService.getUnrealizedPnl(ammAddress, this.wallet, PnlCalcOption.SPOT_PRICE)
+    }
+
+    async getPerpPositonWithUnrealizedPnl(ammAddress: string): Promise<[Position, Big]> {
+        return await Promise.all([
+            this.getPerpPosition(ammAddress),
+            this.getPerpUnrealizedPnl(ammAddress),
+        ])
+    }
+
+    async printPosition(ammAddress: string, ammPair: string): Promise<void> {
+        const [position, unrealizedPnl] = await this.getPerpPositonWithUnrealizedPnl(ammAddress)
+        this.printPerpPosition(ammPair, position, unrealizedPnl)
+    }
+
+    private printPerpPosition(ammPair: string, position: Position, unrealizedPnl?: Big) {
+        let params = {
+            ammPair,
+            size: +position.size.round(5),
+            margin: +position.margin.round(2),
+            openNotional: +position.openNotional.round(2),
+        }
+        
+        if (unrealizedPnl) {
+            params = {...params, ...{unrealizedPnl: +unrealizedPnl.round(2)}}
+        }
+        this.log.jinfo({
+            event: "PerpPosition",
+            params: params,
+        })
+    }    
+}
--- src/ServerProfile.ts
+++ src/ServerProfile.ts
@@ -7,34 +7,19 @@ export class ServerProfile {
     private readonly log = Log.getLogger(ServerProfile.name)
     readonly web3Endpoint: string
     readonly web3EndpointRO: string
-    readonly arbitrageurPK: string
-    // FTX
-    readonly ftxApiKey: string
-    readonly ftxApiSecret: string
-    readonly ftxSubaccount: string | undefined
-    // binance
-    readonly bncApiKey: string = ""
-    readonly bncApiSecret: string = ""
+    readonly walletPrivateKey: string
 
     constructor(obj?: Partial<ServerProfile>) {
         if (obj == null) {
             // if nothing specified, get from env vars
             this.web3Endpoint = process.env.WEB3_ENDPOINT!
             this.web3EndpointRO = process.env.WEB3_ENDPOINT_RO!
-            this.arbitrageurPK = process.env.ARBITRAGEUR_PK!
-            this.ftxApiKey = process.env.FTX_API_KEY!
-            this.ftxApiSecret = process.env.FTX_API_SECRET!
-            this.ftxSubaccount = process.env.FTX_SUBACCOUNT
-            this.bncApiKey = process.env.BINANCE_API_KEY
-            this.bncApiSecret = process.env.BINANCE_API_SECRET
+            this.walletPrivateKey = process.env.ARBITRAGEUR_PK!
         } else {
             // for tools, don't need to have env vars defined
             this.web3Endpoint = ""
             this.web3EndpointRO = ""
-            this.arbitrageurPK = ""
-            this.ftxApiKey = ""
-            this.ftxApiSecret = ""
-            this.ftxSubaccount = ""
+            this.walletPrivateKey = ""
             Object.assign(this, obj)
         }
 
