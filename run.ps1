c272d9ed9225172e8e5cea5890181c856e6fd666
d5shen <darryl.shen+github@gmail.com>
2021-09-14 16:51:33 -0400

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,6 +1,6 @@
 do{
     $ts = Get-Date -Format "yyyy-MM-dd-HH.mm"
     $PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
-    Invoke-Expression "env `$(cat .env.production | grep -v '#') npx ts-node --files src/index.ts"
+    Invoke-Expression "env `$(cat .env.production | grep -v '#') npx ts-node --files src/index.ts >> .\log\$($ts).log"
     Start-Sleep -Milliseconds 500
 } while($true)
