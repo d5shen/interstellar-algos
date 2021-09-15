a3a41554a1d52400f374f8c355fed4a3e9815bdd
d5shen <darryl.shen+github@gmail.com>
2021-09-14 20:53:53 -0400

    Update configs.ts

--- src/configs.ts
+++ src/configs.ts
@@ -5,8 +5,8 @@ import Big from "big.js"
  *  Global Configs, not pair-specific
  *********************************************************/
 
-export const pollFrequency = 3 // in seconds
-export const slowPollFrequency = 30 // in seconds
+export const pollFrequency = 15 // in seconds
+export const slowPollFrequency = 60 // in seconds
 export const perpfiFee = Big(0.001) // default 0.1%
 
 export const configPath = "configs.json"
